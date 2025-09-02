dca9cbcff6c38c9b878bde12a31b1eac7ed9b4a2
longpham28 <44454827+longpham28@users.noreply.github.com>
2025-09-02 20:42:05 +0900

    feat: first build

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pre-trained-model-retrieval-github-io!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/index.md
@@ -0,0 +1,5 @@
+# Welcome to NTCIR-19 Pre-trained Model Retrieval Task
+
+!!! info
+
+    We will update contents soon!
